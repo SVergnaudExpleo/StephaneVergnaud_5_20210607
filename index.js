a333fff8b3e862de749c54c766ad7ae0abaa4156
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-06-14 17:59:29 +0200

    feat(filtre) creation du filtre des ingredients

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 //constante du DOM ingredient
 const switchOnIngredient = document.getElementById("switch-on-ingredient");
 const switchOffIngredient = document.getElementById("switch-off-ingredient");
-const zoneFiltreIngredient = document.getElementById("zone-filtre-ingredient");
-const dropdownmenuIngredient = document.getElementById("dropdown-menu-ingredient");
-const dropdownRechercheIngredient = document.getElementById("dropdown-recherche-ingredient")
+const filtreDeveloppeIngredient = document.getElementById("filtre-developpe-ingredient");
 
 
 // initialiser la mise page avec JS
-zoneFiltreIngredient.style.display = "none";
+filtreDeveloppeIngredient.style.display = "none";
 
 // afficher et masquer le filtre
 switchOnIngredient.addEventListener("click",function(){
     switchOnIngredient.style.display = "none";
-    zoneFiltreIngredient.style.display = "grid";
+    filtreDeveloppeIngredient.style.display = "initial";
     
     
 });
 switchOffIngredient.addEventListener("click",function(){
-    switchOnIngredient.style.display = "initial";
-    zoneFiltreIngredient.style.display = "none";
+    switchOnIngredient.style.display = "flex";
+    filtreDeveloppeIngredient.style.display = "none";
 });
-
-// redimentionner la zone recherche
-zoneFiltreIngredient.style.width = dropdownmenuIngredient.clientWidth;
