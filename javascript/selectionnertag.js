56daaeb281ffc11bbac34c47a34e93931c0feb93
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-06-22 10:20:43 +0200

    hotfix(tag) maj algorithme de selection

--- javascript/selectionnertag.js
+++ javascript/selectionnertag.js
@@ -2,21 +2,21 @@
 function actualiserListeTag(){
     return document.querySelectorAll(".far.fa-times-circle");
 };
-// Fonction créer un tag ingredient
+// Fonction créer un tag depuis une liste de tag
 function creerTag(param){
     const nouvDiv = document.createElement("div");
     const divTagValeur = document.createElement("div");
     const nouvLogo = document.createElement("i");
     const idParam = param.parentElement.id;
-    if (idParam == "liste-recherche-ingredient") {
+    if (idParam == "liste-recherche-ingredient"){
         nouvDiv.className = "tag tag--ingredient";
-    }else if (idParam == "liste-recherche-appareil") {
+    }else if (idParam == "liste-recherche-appareil"){
         nouvDiv.className = "tag tag--appareil";
     }else if (idParam == "liste-recherche-ustensil"){
         nouvDiv.className = "tag tag--ustensil";
-    }
+    };
     divTagValeur.className = "valeur-tag";
-    divTagValeur.textContent = param.textContent; //injecter la valeur du tag selectionné
+    divTagValeur.textContent = param.textContent;
     nouvLogo.className = "far fa-times-circle";
     tagBox.appendChild(nouvDiv);
     nouvDiv.appendChild(divTagValeur);
@@ -27,11 +27,11 @@ function fermerTag(){
     actualiserListeTag().forEach(function(item){
         item.addEventListener('click', function(event){
             event.stopPropagation();
-            item.parentElement.style.display = "none"
+            item.parentElement.style.display = "none";
         })
     });
 };
-// Créer un tag ingredient
+// Ecouter la selection du tag dans la liste 
 listeTag.forEach(function(tag){
     tag.forEach(function(item){
         item.addEventListener("click",function(event){
