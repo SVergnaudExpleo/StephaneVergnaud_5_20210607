f0e43adf8669d0337df49123d9da7b8c477dfb6b
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-06-30 14:34:10 +0200

    feat(recherche) creation JS gérer la barre de recherche globale

--- javascript/dev.js
+++ javascript/dev.js
@@ -1,28 +1,65 @@
 
+/* Reflexion sur code => a supprimer 
+-------------------------------------------------*/
+
 // déterminer le cas de recherche
     //si 3 caractère alors trier => retourner recette trier
     //si 3 caractere et tri par tag effectué alors trier les recette tagées
     //si pas 3 caractère alors pas trier => retourner la liste reciepes
         // et gérer la recherche par tag
     
-
 // fonction recherche globale
 // fonction créer un tableau filtrer globalement
 // fonction recherche par tag
 // fonction créer un tableau filtré globalement et par tag
 
-
-
-
-
-
 /*
-//choisir le tableau à trier
-function choixTableTriTag(){
-    if (rechGlobale.textContent.length >= 3){
-        return "recipes";
-    }else{
-        return "recipes"; // mettre le tableau retourner de recherche globale
-    };
+-------------------------------------------------*/
+
+// Recherche barre de recherche globale
+function rechercheGlobale(listeATrier){
+    rechGlobale.addEventListener("input",function(){
+        var recetteTrieeGlobalBrut = [];
+        var recetteTrieeGlobal = [];
+        if (rechGlobale.value.length >= 3){
+            var valeurChercher = rechGlobale.value.trim()
+            var valRechRegex = new RegExp(valeurChercher, 'i');
+            listeATrier.forEach(function(uneRecette){
+                if (uneRecette.name.search(valRechRegex) > -1){
+                    recetteTrieeGlobalBrut.push(uneRecette);
+                };
+                uneRecette.ingredients.forEach(function(unIngredient){
+                    if (unIngredient.ingredient.search(valRechRegex) > -1){
+                        recetteTrieeGlobalBrut.push(uneRecette);
+                    };
+                });
+                if (uneRecette.description.search(valRechRegex) > -1){
+                    recetteTrieeGlobalBrut.push(uneRecette);
+                };
+            });
+            if (recetteTrieeGlobalBrut.length > 0){
+                recetteTrieeGlobal = supprimerDoublon(recetteTrieeGlobalBrut);
+                carteBox.innerText = "";
+                creerListeCarteRecette(recetteTrieeGlobal);
+            }else{
+                recetteTrieeGlobal = recetteTrieeGlobalBrut;
+                pasDeResultat();
+            };
+        }else if (rechGlobale.value.length < 3) {
+            carteBox.innerText="";
+            recetteTrieeGlobal = listeATrier;
+            creerListeCarteRecette(listeATrier);
+        };
+        console.log(recetteTrieeGlobal);
+        return recetteTrieeGlobal;
+    }); 
 };
-*/
+
+function pasDeResultat(){
+    var carteListe = document.querySelectorAll(".carte");
+    carteListe.forEach(function(carte){
+        carte.remove();
+    });
+    carteBox.innerText = "PAS DE RESULTAT POUR VOTRE RECHERCHE"
+}
+
